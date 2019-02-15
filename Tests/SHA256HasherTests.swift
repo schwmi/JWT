31de7a1048ef452b66dab8ea2f7219381ecb4c0b
Michael Schwarz <michael_schwarz@me.com>
2019-02-15 21:05:25 +0100

    Add additional hashing test

--- Tests/SHA256HasherTests.swift
+++ Tests/SHA256HasherTests.swift
@@ -5,13 +5,25 @@ import XCTest
 final class SHA256HasherTests: XCTestCase {
 
     func testHashCreation() {
-        guard let hash = try? SHA256Hasher().hash(from: "TestString") else {
-            XCTFail("Expected a hash")
-            return
+        do {
+            guard let hash = try? SHA256Hasher().hash(from: "TestString") else {
+                XCTFail("Expected a hash")
+                return
+            }
+
+            let hex = hash.map { String(format: "%02X", $0) }.joined()
+            // check with hash created at https://passwordsgenerator.net/sha256-hash-generator/
+            XCTAssertEqual(hex, "6DD79F2770A0BB38073B814A5FF000647B37BE5ABBDE71EC9176C6CE0CB32A27")
         }
 
-        let hex = hash.map { String(format: "%02X", $0) }.joined()
-        // check with hash created at https://passwordsgenerator.net/sha256-hash-generator/
-        XCTAssertEqual(hex, "6DD79F2770A0BB38073B814A5FF000647B37BE5ABBDE71EC9176C6CE0CB32A27")
+        do {
+            let digest = "eyJhbGciOiJFUzI1NiIsImtpZCI6IjI3S1hQWEs5WlAiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiI2OWE2ZGU5OC0zNDYyLTQ3ZTMtZTA1My01YjhjN2MxMWE0ZDEiLCJleHAiOjE1NTAyNjIxNzEuMzc1NjAzLCJhdWQiOiJhcHBzdG9yZWNvbm5lY3QtdjEifQ"
+            guard let hash = try? SHA256Hasher().hash(from: digest) else {
+                XCTFail("Expected a hash")
+                return
+            }
+            let base64Hash = Data(hash).base64EncodedString()
+            XCTAssertEqual(base64Hash, "7bffigO0Ivy7PwlbHpVHPjenrDfzg3354xJA2CIHcww=")
+        }
     }
 }
